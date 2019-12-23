1ba3ae86bd84c2366fe4982d146e8ed3522323e5
CORE-I7-7700K\John <eviljohn@gmail.com>
2019-12-23 16:59:29 -0600

    Latest Tank movement changes.

--- BattleTank/Source/BattleTank/TankMovementComponent.h
+++ BattleTank/Source/BattleTank/TankMovementComponent.h
@@ -20,21 +20,22 @@ class BATTLETANK_API UTankMovementComponent : public UNavMovementComponent
 public:
 
 	// Makes a tank movement intention
-	UFUNCTION(BlueprintCallable, Category = Input)
+	UFUNCTION(BlueprintCallable, Category = "Input")
 	void IntendMoveForward(float Throw);
 
 	// Turn the tank to the Right
-	UFUNCTION(BlueprintCallable, Category = Input)
+	UFUNCTION(BlueprintCallable, Category = "Input")
 	void IntendTurnRight(float Throw);
 
 	// Sets the movement meshes for the movement component
-	UFUNCTION(BlueprintCallable, Category = Setup)
+	UFUNCTION(BlueprintCallable, Category = "Setup")
 	void Initialize(UTankTrack* LeftTrackToSet, UTankTrack* RightTrackToSet);
 
+protected:
 	/** path following: request new velocity */
 	virtual void RequestDirectMove(const FVector& MoveVelocity, bool bForceMaxSpeed) override;
 
-protected:
+
 	UTankTrack* LeftTrack = nullptr;
 	UTankTrack* RightTrack = nullptr;
 
